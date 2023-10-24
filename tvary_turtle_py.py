e811d4294140ec2cf8c3b978b866d795645e03c5
jan <janplatkevic@gmail.com>
2023-10-24 18:17:13 +0200

    a

---
+++ tvary_turtle_py.py
@@ -0,0 +1,30 @@
+from turtle import *
+speed(int (textinput("rychlost", " ")))
+def otazka():
+    a=textinput("jaky chces tvar?", " ")
+    while a!=("kytka") and a!=("ctverec") and a!=("kruh"):
+        a=textinput("jaky chces tvar?", " ")
+    return a
+def kytka():
+    for i in range(9):
+        for i in range(15):
+            forward(15)
+            right(5)
+        right(85)
+def ctverec():
+    for i in range(4):
+        forward(50)
+        left(90)
+def kruh():
+    for i in range(75):
+        forward(10)
+        right(5)
+def otazka_i_to_ostatni():
+    a=otazka()
+    if a==("kytka"):
+      kytka()
+    elif a==("ctverec"):
+        ctverec()
+    elif a==("kruh"):
+        kruh()
+    exitonclick()
