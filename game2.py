e16b4adf7bb8f65d058d9e483b5fb38ce46427e8
jan <janplatkevic@gmail.com>
2024-01-16 18:21:30 +0100

    hra:burger

--- game2.py
+++ game2.py
@@ -1,8 +1,9 @@
 mistnosti = ["obývák", "chodba", "sklep", "trůnní sál","jídelna"]
 chodby = [[1, 2,4], [0,4], [0], [1, 2],[1,0]]
 zamcene_chodby = [[], [3], [], [],[]]
-inventar = {"klic" : False}
-sytost=5
+inventar = {"klic" : False, "moje zlato":0,"burger":False}
+cena_burgeru=15
+sytost=4
 mistnost_s_klicem = 2
 zlato = [1, 0, 10, 300,15]
 hrac = 0
@@ -19,6 +20,8 @@ def hotovo():
     return sum(zlato) == 0
 while not hotovo():
     print("mas sytost",sytost)
+    if inventar["burger"]:
+        print("mas burger")
     print("hráč je v místnosti:", mistnosti[hrac])
     print("hráč má", skore, "zlata")
     print("zbývá zlata:", sum(zlato))
@@ -32,8 +35,10 @@ while not hotovo():
         print("Moznost C : sebrat klíč")
     if inventar["klic"] and zamcene_chodby[hrac]:
         print("Moznost R : odemknout dveře ->", mistnosti[zamcene_chodby[hrac][-1]])
-    if hrac == 4:
+    if hrac == 4 or inventar["burger"]:
         print("moznost J: najíst se")
+    if hrac == 4 and inventar["moje zlato"] >= cena_burgeru:
+        print("prodavačka říká:Chceš burger?řekni b")
 
     vstup_ok=False
     while not vstup_ok:
@@ -42,21 +47,28 @@ while not hotovo():
             vstup_ok=True
         elif zlato[hrac] and vstup=="x":
             vstup_ok=True
-        elif hrac==4 and vstup=="j":
+        elif (hrac==4 or inventar["burger"]) and vstup=="j":
             vstup_ok=True
         elif vstup=="r" and hrac==1 and zamcene_chodby[hrac]:
+            vstup_ok=True
+        elif hrac == 4 and inventar["moje zlato"] >= cena_burgeru and not inventar["burger"] and vstup=="b":
             vstup_ok=True
         elif je_cislo(vstup) and int(vstup) <= len(kam_lze_jit) and int(vstup) > 0:
             vstup_ok=True
 
     if vstup == "x":
         skore += zlato[hrac]
+        inventar["moje zlato"]+=zlato[hrac]
         zlato[hrac] = 0
     elif vstup == "c":
         inventar["klic"] = True
         mistnost_s_klicem = -1
-    elif hrac==4 and vstup=="j":
+    elif vstup=="j":
         sytost=11
+        if not hrac==4:
+            inventar["burger"]=False
+    elif vstup == "b":
+        inventar["burger"]=True
 
     elif vstup == "r":
         cilova_mistnost = zamcene_chodby[hrac].pop()
