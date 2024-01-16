8606e0ccf87394d249d0850c92c36b4d0e711e00
jan <janplatkevic@gmail.com>
2024-01-16 17:26:52 +0100

    hra:chodba byvak-jidelna

--- game2.py
+++ game2.py
@@ -1,11 +1,9 @@
 mistnosti = ["obývák", "chodba", "sklep", "trůnní sál","jídelna"]
-chodby = [[1, 2], [0,4], [0], [1, 2],[1]]
+chodby = [[1, 2,4], [0,4], [0], [1, 2],[1,0]]
 zamcene_chodby = [[], [3], [], [],[]]
 sytost=5
 klic = 2
-
 zlato = [1, 0, 10, 300,15]
-
 hrac = 0
 skore = 0
 kroky = 0
@@ -18,30 +16,23 @@ def je_cislo(mozna_cislo):
         return False
 def hotovo():
     return sum(zlato) == 0
-
 while not hotovo():
     print("mas sytost",sytost)
     print("hráč je v místnosti:", mistnosti[hrac])
     print("hráč má", skore, "zlata")
     print("zbývá zlata:", sum(zlato))
-
     kam_lze_jit = chodby[hrac]
+
     for i, moznost in enumerate(kam_lze_jit):
         print("Moznost", i + 1, ": ", mistnosti[moznost])
-
     if zlato[hrac] > 0:
         print("Moznost X : sebrat", zlato[hrac], "zlata")
-
     if hrac == klic:
         print("Moznost C : sebrat klíč")
-
     if ma_klic and zamcene_chodby[hrac]:
         print("Moznost R : odemknout dveře ->", mistnosti[zamcene_chodby[hrac][-1]])
-
     if hrac == 4:
         print("moznost J: najíst se")
-
-
 
     vstup_ok=False
     while not vstup_ok:
@@ -52,12 +43,10 @@ while not hotovo():
             vstup_ok=True
         elif hrac==4 and vstup=="j":
             sytost=10
-
         elif vstup=="r" and hrac==1 and zamcene_chodby[hrac]:
             vstup_ok=True
         elif je_cislo(vstup) and int(vstup) <= len(kam_lze_jit) and int(vstup) > 0:
             vstup_ok=True
-
 
     if vstup == "x":
         skore += zlato[hrac]
@@ -71,10 +60,10 @@ while not hotovo():
         print("Slyšíš jak cvaknul zámek a dveře se otevřely.")
     else:
         hrac = kam_lze_jit[int(vstup) - 1]
+
     sytost-=1
     kroky += 1
     if sytost == 0:
         print("umřels hlady")
         break
-
 print("Gratuluju, sebral jsi celkem,", skore, "zlata za", kroky, "kroků")
