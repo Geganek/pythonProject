88817aadf00551c14c4b06a135b20a33736a1d59
jan <janplatkevic@gmail.com>
2024-03-12 18:30:33 +0100

    hra: oprava hadani

---
+++ lokace.py
@@ -0,0 +1,18 @@
+class Hrad:
+    mistnosti = ["obývák", "chodba", "sklep", "trůnní sál","jídelna","dedeckova chajda"]
+    chodby = [[1, 2,4], [0,4,5], [0,5], [1, 2],[1,0,5],[ 1,2,4]]
+    zamcene_chodby = [[], [3], [], [],[],[]]
+    mistnost_s_klicem = 2
+    cena_burgeru = 15
+    zlato = [1, 0, 10, 300,15,0]
+    jmeno="tajemny hrad"
+
+class Posta:
+    mistnosti = ["vchod", "pruchod", "zachod", "odesilarna","kantina"]
+    chodby = [[1, 2,4], [0,4], [0], [1, 2],[1,0]]
+    zamcene_chodby = [[], [3], [], [],[]]
+    mistnost_s_klicem = 2
+    cena_burgeru = 20
+    zlato = [1, 0, 10, 300,15]
+    jmeno="hlavni posta"
+print()
