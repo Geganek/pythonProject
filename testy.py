cd7477cb52911922aafb1f9051e552013e95f2ee
jan <janplatkevic@gmail.com>
2024-04-16 18:33:25 +0200

    tyrideni

--- testy.py
+++ testy.py
@@ -1,32 +1,68 @@
-mistnosti = ["0-obývák", "1-chodba", "2-sklep", "3-trůnní sál","4-jídelna","5-terasa","6-půda","7-kumbál","8-sídlo","9-ložnice","10-chodba","11-tajemna komnata"]
-chodby = [[1, 2,4], [0,4,6], [0,11], [1, 2],[1,0,5,7],[4,6,10],[1,5],[4],[10],[10],[9,8,5],[2]]
+import random
+import time
+random.seed(0)
+seznam_cisel = []
+maximum = 100
+pocet = 100
 
-start = 0
-konec = int(input("cil?"))
+for i in range(pocet):
+    seznam_cisel.append(random.randint(0, maximum))
 
-pozice = start
-navstiveno = [start]
-nalezena_cesta = None
-pozice = start
-cesty = [[start]]
+print(seznam_cisel)
 
-while cesty:
-    cesta = cesty.pop(0)
-    posledni_mistnost = cesta[-1]
-    moznosti = []
+def setrid(seznam):
 
-    for m in chodby[posledni_mistnost]:
-        if m not in cesta:
-            moznosti.append(m)
+    prohozeno = True
+    while prohozeno:
+        prohozeno = False
 
-    if konec == posledni_mistnost:
-        nalezena_cesta=cesta
-        break
+        for i in range(len(seznam) - 1):
+            a = seznam[i]
+            b = seznam[i + 1 ]
+            if a > b:
+                seznam[i] = b
+                seznam[i + 1] = a
+                prohozeno = True
 
-    for m in moznosti:
-        cesty.append(cesta + [m])
+def setridlepe (seznam):
+    if len(seznam) == 1:
+        return seznam
+    prvni_pulka = setridlepe(seznam[:len(seznam)//2])
+    druha_pulka = setridlepe(seznam[len(seznam)//2:])
 
-    print(cesty)
-print("nejkratsi cesta:",nalezena_cesta)
+    i,j =0,0
+    setrideny_seznam = []
+
+    while i < len(prvni_pulka) or j < len(druha_pulka) :
+        if i == len(prvni_pulka):
+            setrideny_seznam.extend(druha_pulka[j:])
+            break
+
+        if j == len(druha_pulka):
+            setrideny_seznam.extend(prvni_pulka[i:])
+            break
+
+        a = prvni_pulka[i]
+        b = druha_pulka[j]
+
+        if a < b:
+            setrideny_seznam.append(a)
+            i += 1
+        else:
+            j += 1
+            setrideny_seznam.append(b)
+
+    return(setrideny_seznam)
 
 
+
+
+
+
+start = time.time()
+seznam = setridlepe(seznam_cisel)
+konec = time.time()
+
+
+print(seznam)
+print("trvalo to",konec - start,"sekund")
